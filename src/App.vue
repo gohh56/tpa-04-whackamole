36b75d7230454cccb2893d08acf3bd8a64c8be77
gohh56 <ystokio@gmail.com>
2018-12-19 14:58:44 +0900

    done checkpoint-1

--- src/App.vue
+++ src/App.vue
@@ -9,51 +9,29 @@
       Start Game
     </button>
     <div class="counters-container">
-      <div class="counter">
-        <h2>Score:</h2>
-        <h1>0</h1>
-      </div>
-      <div class="counter">
-        <h2>High Score:</h2>
-        <h1>0</h1>
-      </div>
-      <div class="counter">
-        <h2>Timer:</h2>
-        <h1>0</h1>
-      </div>
+      <Counter title="Score:"></Counter>
+      <Counter title="High Score:"></Counter>
+      <Counter title="Timer:"></Counter>
     </div>
     <div class="moles-container gameActive">
-      <div class="mole-container inactive">
-        <div class="mole-image-container">
-          <img class="mole" src="./assets/mole.png" alt="mole"/>
-        </div>
-        <img class="dirt" src="./assets/dirt.svg" alt="mole dirt"/>
-      </div>
-      <div class="mole-container inactive">
-        <div class="mole-image-container">
-          <img class="mole" src="./assets/mole.png" alt="mole"/>
-        </div>
-        <img class="dirt" src="./assets/dirt.svg" alt="mole dirt"/>
-      </div>
-      <div class="mole-container inactive">
-        <div class="mole-image-container">
-          <img class="mole" src="./assets/mole.png" alt="mole"/>
-        </div>
-        <img class="dirt" src="./assets/dirt.svg" alt="mole dirt"/>
-      </div>
-      <div class="mole-container inactive">
-        <div class="mole-image-container">
-          <img class="mole" src="./assets/mole.png" alt="mole"/>
-        </div>
-        <img class="dirt" src="./assets/dirt.svg" alt="mole dirt"/>
-      </div>
+      <MolesContainer></MolesContainer>
+      <MolesContainer></MolesContainer>
+      <MolesContainer></MolesContainer>
+      <MolesContainer></MolesContainer>
     </div>
   </div>
 </template>
 
 <script>
+import Counter from './components/Counter.vue';
+import MolesContainer from './components/MolesContainer.vue';
+
 export default {
   name: 'App',
+  components: {
+    Counter,
+    MolesContainer,
+  }
 };
 </script>
 
@@ -81,17 +59,6 @@ export default {
   justify-content: space-evenly;
 }
 
-.counter {
-  border: 1px solid #000;
-  margin-top: 20px;
-  padding: 20px;
-}
-
-.counter h1,
-.counter h2 {
-  margin: 0;
-}
-
 .moles-container {
   margin-top: 20px;
   display: flex;
@@ -102,48 +69,5 @@ export default {
 
 .moles-container.game-active {
   opacity: 1;
-}
-
-.mole-container {
-  width: 160px;
-  height: 160px;
-  display: inline-block;
-  margin: 10px;
-  position: relative;
-}
-
-.mole-image-container {
-  overflow: hidden;
-  width: 160px;
-  height: 140px;
-}
-
-.mole-container img {
-  display: block;
-  transition: all 0.3s ease;
-}
-
-.mole {
-  position: relative;
-  width: 60%;
-  margin: auto;
-  cursor: pointer;
-}
-
-.mole-container.active .mole {
-  /* display: block; */
-  top: 0px;
-}
-
-.mole-container.inactive .mole {
-  top: 200px;
-}
-
-.dirt {
-  width: 100%;
-  margin: auto;
-  z-index: 1;
-  position: absolute;
-  bottom: 0;
 }
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="counter">
+    <h2>{{ title }}</h2>
+    <h1>{{ counter }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props: ['title'],
+  data: function() {
+    return {
+      counter: 0,
+    };
+  }
+};
+</script>
+
+<style scoped>
+.counter {
+  border: 1px solid #000;
+  margin-top: 20px;
+  padding: 20px;
+}
+
+.counter h1,
+.counter h2 {
+  margin: 0;
+}
+</style>
+
